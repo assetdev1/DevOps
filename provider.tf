e8de800074606bdaa109449fbdbdcafd4c281f27
assetdev1 <141229960+assetdev1@users.noreply.github.com>
2023-11-08 13:24:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,12 +5,7 @@ terraform {
      version = "~> 5.0"
    }
  }
-provider "aws" {
-  access_key = "AKIAULUFSX7KSKXUINYV"
-  secret_key = "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"
-  region     = "ap-south-2"
-}
-
+ 
  backend "s3" {
    bucket = "assetdev-jenkins"
    key    = "state"
