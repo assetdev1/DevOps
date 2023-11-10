abeafd9dbc3fabb4b83079ee3e35415e476d53a9
assetdev1 <141229960+assetdev1@users.noreply.github.com>
2023-11-10 17:04:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,1 @@
-variable "ami" {
-   type        = string
-   description = "Amazon linux in hyderabad region"
-   default     = "ami-0b8b346a5e8457d66"
-}
 
-variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t3.micro"
-}
-
-variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-   default     = "testing-jenkins"
-}
