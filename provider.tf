dacbfe6403a3f384ae9302f5ba032eb2323326bd
assetdev1 <141229960+assetdev1@users.noreply.github.com>
2023-11-10 17:04:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,1 @@
-terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 5.0"
-     
-   }
- }
 
- backend "s3" {
-   bucket = "assetdev-jenkins"
-   key    = "state"
-   region = "ap-south-2"
- }
-}
